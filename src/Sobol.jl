b17815ef7dd083df89dc07ecef83cae9dc3dc0a8
Ken-B <ken.bastiaensen@gmail.com>
2014-08-20 22:30:20 +0200

    Pure Julia implementation of SobolSeq
    adaptation after @stevengj feedback:
    force Uint32; devectorize some assignments; change skip number
    
    Uint32 -> Uint tryout
    
    make it work for julia v0.4 and UInt32
    
    support 64bit, make it work for general UInt
    
    Revert "support 64bit, make it work for general UInt"
    
    This reverts commit 94099ad384f8c6b9811905d99061dd309687c96c.
    
    make sdims a type parameter, allow sdims==0, compatibility with v0.3 and minor changes from comments
    
    add recompilation
    
    remove special case next for N==0
    
    update README and small incrementer fix

--- src/Sobol.jl
+++ src/Sobol.jl
@@ -1,67 +1,97 @@
-module Sobol
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
 
-import NLopt.libnlopt
-import Base: ndims, length, skip, next, start, done, show, writemime
+module Sobol
+using Compat
+
+import Base: ndims, skip, next, start, done, show, writemime
 export SobolSeq, ScaledSobolSeq, next!
 
-type SobolSeq{N}
-    p::Ptr{Void} # nlopt_sobol pointer
-    function SobolSeq()
-        (N < 1 || N > 1111) && error("invalid Sobol dimension")
-        p = ccall((:nlopt_sobol_create,libnlopt), Ptr{Void}, (Cuint,), N)
-        p == C_NULL && error("error initializing Sobol sequence")
-        s = new(p)
-        finalizer(s, destroy)
-        s
-    end
-end
-SobolSeq(N::Integer) = SobolSeq{N}()
+include("soboldata.jl") #loads `sobol_a` and `sobol_minit`
 
-destroy(s::SobolSeq) = 
-    ccall((:nlopt_sobol_destroy,libnlopt), Void, (Ptr{Void},), s.p)
+# N iis the dimension of sequence being generated
+type SobolSeq{N}
+    m::Array{UInt32,2} #array of size (sdim, 32)
+    x::Array{UInt32,1} #previous x = x_n, array of length sdim
+    b::Array{UInt32,1} #position of fixed point in x[i] is after bit b[i]
+    n::UInt32 #number of x's generated so far
+end
 
 ndims{N}(s::SobolSeq{N}) = N
 
-function show{N}(io::IO, s::SobolSeq{N})
-    print(io, "$N-dimensional Sobol sequence on [0,1]^$N")
-end
-function writemime{N}(io::IO, ::MIME"text/html", s::SobolSeq{N})
-    print(io, "$N-dimensional Sobol sequence on [0,1]<sup>$N</sup>")
+function SobolSeq(N::Int)
+    (N < 0 || N > 1111) && error("invalid Sobol dimension")
+
+    m = ones(UInt32, (N, 32))
+
+    #special cases
+    N == 0 && return(SobolSeq{0})(m,UInt32[],UInt32[],zero(UInt32))
+    #special cases 1
+    N == 1 && return(SobolSeq{N}(m,UInt32[0],UInt32[0],zero(UInt32)))
+
+    for i = 2:N
+        a = sobol_a[i-1]
+        d = floor(Int, log2(a)) #degree of poly
+
+        #set initial values of m from table
+        # TODO transpose sobol_minit for faster column access?
+        m[i, 1:d] = sobol_minit[i-1, 1:d]
+        #fill in remaining values using recurrence
+        for j = (d+1):32
+            ac = a
+            m[i,j] = m[i,j-d]
+            for k = 0:d-1
+                m[i,j] $= ((ac & one(UInt32)) * m[i, j-d+k]) << (d-k)
+                ac >>= 1
+            end
+        end
+    end
+    SobolSeq{N}(m,zeros(UInt32,N),zeros(UInt32,N),zero(UInt32))
 end
 
-function skip!{N}(s::SobolSeq{N}, n::Integer, x::Vector{Float64})
-    (n < 0 || length(x) < N) && throw(BoundsError())
-    ccall((:nlopt_sobol_skip,libnlopt), Void, (Ptr{Void}, Cuint, Ptr{Float64}),
-          s.p, n, x)
-    x
+function next!{T<:FloatingPoint}(s::SobolSeq, x::AbstractVector{T})
+    length(x) != ndims(s) && throw(BoundsError())
+
+    if s.n == typemax(s.n)
+        rand!(x)
+        return nothing
+    end
+
+    s.n += one(s.n)
+    c = @compat UInt32(trailing_zeros(s.n))
+    for i=1:ndims(s)
+        b = s.b[i]
+        if b >= c
+            s.x[i] $= s.m[i,c+1] << (b-c)
+            x[i] = s.x[i] / (one(UInt32) << (b + one(UInt32)))
+        else
+            s.x[i] = (s.x[i] << (c-b)) $ s.m[i,c+1]
+            s.b[i] = c
+            x[i] = s.x[i] / (one(UInt32) << (c + one(UInt32)))
+        end
+    end
+    return x
+end
+next(s::SobolSeq) = next!(s, Array(Float64,ndims(s)))
+
+# if we know in advance how many points (n) we want to compute, then
+# adopt the suggestion of the Joe and Kuo paper, which in turn
+# is taken from Acworth et al (1998), of skipping a number of
+# points equal to the largest power of 2 smaller than n
+function skip(s::SobolSeq, n::Integer)
+    nskip = 1 << floor(Int,log2(n))
+    for unused=1:nskip; next(s); end
+    return nothing
 end
 
-function skip{N}(s::SobolSeq{N}, n::Integer) 
-    skip!(s, n, Array(Float64, N))
-    nothing
+function show(io::IO, s::SobolSeq)
+    print(io, "$(ndims(s))-dimensional Sobol sequence on [0,1]^$(ndims(s))")
 end
-
-function next!{N}(s::SobolSeq{N}, x::Vector{Float64})
-    length(x) < N && throw(BoundsError())
-    ccall((:nlopt_sobol_next01,libnlopt), Void,
-          (Ptr{Void}, Ptr{Float64}), s.p, x)
-    x
+function writemime(io::IO, ::MIME"text/html", s::SobolSeq)
+    print(io, "$(ndims(s))-dimensional Sobol sequence on [0,1]<sup>$(ndims(s))</sup>")
 end
-next{N}(s::SobolSeq{N}) = next!(s, Array(Float64,N))
-
-function next!{N}(s::SobolSeq{N}, x::Vector{Float64},
-                  lb::Vector, ub::Vector)
-    length(x) < N && throw(BoundsError())
-    ccall((:nlopt_sobol_next,libnlopt), Void,
-          (Ptr{Void}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}), 
-          s.p, x, float64(lb), float64(ub))
-    x
-end
-next{N}(s::SobolSeq{N}, lb::Vector, ub::Vector) =
-    next!(s, Array(Float64,N), lb, ub)
 
 # Make an iterator so that we can do "for x in SobolSeq(...)".
-# Technically, the Sobol sequence ends after 2^32-1 points, but NLopt
+# Technically, the Sobol sequence ends after 2^32-1 points, but it
 # falls back on pseudorandom numbers after this.  In practice, one is
 # unlikely to reach that point.
 start(s::SobolSeq) = s
@@ -70,18 +100,31 @@ done(s::SobolSeq, s_::SobolSeq) = false
 
 # Convenience wrapper for scaled Sobol sequences
 
-type ScaledSobolSeq{N}
-    s::SobolSeq{N}
+type ScaledSobolSeq
+    s::SobolSeq
     lb::Vector{Float64}
     ub::Vector{Float64}
-    ScaledSobolSeq(lb::Vector{Float64}, ub::Vector{Float64}) =
+    ScaledSobolSeq(N::Integer, lb::Vector{Float64}, ub::Vector{Float64}) =
         new(SobolSeq(N), lb, ub)
 end
 SobolSeq(N::Integer, lb, ub) =
-    ScaledSobolSeq{N}(copy!(Array(Float64,N),lb), copy!(Array(Float64,N),ub))
+    ScaledSobolSeq(N, copy!(Array(Float64,N),lb), copy!(Array(Float64,N),ub))
+
+ndims(s::ScaledSobolSeq) = ndims(s.s)
+
+function next!(s::SobolSeq, x::Vector{Float64},
+                  lb::Vector, ub::Vector)
+    length(x) < ndims(s) && throw(BoundsError())
+    next!(s,x)
+    for i=1:ndims(s)
+        x[i] = lb[i] + (ub[i]-lb[i]) * x[i]
+    end
+    return x
+end
+next(s::SobolSeq, lb::Vector, ub::Vector) = next!(s, Array(Float64,N), lb, ub)
 
 next!(s::ScaledSobolSeq, x::Vector{Float64}) = next!(s.s, x, s.lb, s.ub)
-next{N}(s::ScaledSobolSeq{N}) = next!(s, Array(Float64,N))
+next(s::ScaledSobolSeq) = next!(s, Array(Float64,ndims(s)))
 
 start(s::ScaledSobolSeq) = s
 next(s::ScaledSobolSeq, s_::ScaledSobolSeq) = (next(s), s_)
@@ -89,8 +132,9 @@ done(s::ScaledSobolSeq, s_::ScaledSobolSeq) = false
 
 skip(s::ScaledSobolSeq, n) = skip(s.s, n)
 
-function show{N}(io::IO, s::ScaledSobolSeq{N})
+function show(io::IO, s::ScaledSobolSeq)
     lb = s.lb; ub = s.ub
+    N = ndims(s)
     print(io, "$N-dimensional scaled Sobol sequence on [$(lb[1]),$(ub[1])]")
     cnt = 1
     for i = 2:N
@@ -109,7 +153,8 @@ function show{N}(io::IO, s::ScaledSobolSeq{N})
     end
 end
 
-function writemime{N}(io::IO, ::MIME"text/html", s::ScaledSobolSeq{N})
+function writemime(io::IO, ::MIME"text/html", s::ScaledSobolSeq)
+    N = ndims(s)
     lb = s.lb; ub = s.ub
     print(io, "$N-dimensional scaled Sobol sequence on [$(lb[1]),$(ub[1])]")
     cnt = 1
@@ -128,5 +173,6 @@ function writemime{N}(io::IO, ::MIME"text/html", s::ScaledSobolSeq{N})
         print(io, "<sup>", cnt, "</sup>")
     end
 end
+
 
 end # module
